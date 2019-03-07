8fd06317db006afc38eeba2471b14a22761cb619
tisoc <tisoc@128.4.134.82>
2019-03-06 19:04:48 -0500

    chris

--- SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
+++ SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
@@ -98,7 +98,6 @@ public class LatinSquare {
 	 *            The column you want returned from the two-dimensional array
 	 * @return one dimensional array of values for the given column
 	 */
-	// Chris
 	public int[] getColumn(int iCol) {
 
 		// TODO: Return a given column from LatinSquare
